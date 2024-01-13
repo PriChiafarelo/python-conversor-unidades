5799a7bc6554fa05a2082b5d54b6784f2c497643
PriChiafarelo <priscilachiafarelo@gmail.com>
2024-01-13 14:33:31 -0300

    primeiro commit

---
+++ desafio008.py
@@ -0,0 +1,3 @@
+"""Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros"""
+valor = float(input('Digite um valor: '));
+print('O valor de {} metros convertido: \nem centímetros é {} \ne {} milímetros'.format(valor, valor*100, valor*1000));
